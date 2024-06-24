25cf999157135bbc76f2f06857ad295847290e6e
winfordguo <winfordguo@gmail.com>
2024-06-24 11:04:56 +0800

    add 'delete remote before uploading' support

--- src/main/java/org/inurl/jenkins/plugin/OSSPublisher.java
+++ src/main/java/org/inurl/jenkins/plugin/OSSPublisher.java
@@ -3,10 +3,15 @@ package org.inurl.jenkins.plugin;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
 
 import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.DeleteObjectsRequest;
+import com.aliyun.oss.model.OSSObjectSummary;
+import com.aliyun.oss.model.ObjectListing;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
@@ -39,6 +44,8 @@ public class OSSPublisher extends Publisher implements SimpleBuildStep {
 
     private final String remotePath;
 
+    private final String deleteRemotePath;
+
     private final String maxRetries;
 
     public String getEndpoint() {
@@ -65,19 +72,24 @@ public class OSSPublisher extends Publisher implements SimpleBuildStep {
         return remotePath;
     }
 
+    public String getDeleteRemotePath() {
+        return deleteRemotePath;
+    }
+
     public int getMaxRetries() {
         return StringUtils.isEmpty(maxRetries) ? 3 : Integer.parseInt(maxRetries);
     }
 
     @DataBoundConstructor
     public OSSPublisher(String endpoint, String accessKeyId, String accessKeySecret, String bucketName,
-            String localPath, String remotePath, String maxRetries) {
+            String localPath, String remotePath, String deleteRemotePath, String maxRetries) {
         this.endpoint = endpoint;
         this.accessKeyId = accessKeyId;
         this.accessKeySecret = Secret.fromString(accessKeySecret);
         this.bucketName = bucketName;
         this.localPath = localPath;
         this.remotePath = remotePath;
+        this.deleteRemotePath = deleteRemotePath;
         this.maxRetries = maxRetries;
     }
 
@@ -92,6 +104,14 @@ public class OSSPublisher extends Publisher implements SimpleBuildStep {
         PrintStream logger = listener.getLogger();
         EnvVars envVars = run.getEnvironment(listener);
         OSSClient client = new OSSClient(endpoint, accessKeyId, accessKeySecret.getPlainText());
+
+        // Delete remote files or directories before uploading
+        if (StringUtils.isNotBlank(deleteRemotePath)) {
+            String[] deleteRemotes = deleteRemotePath.split(",");
+            for (String deleteRemote : deleteRemotes) {
+                delete(client, logger, deleteRemote, true);
+            }
+        }
 
         String local = localPath.substring(1);
         
@@ -161,6 +181,60 @@ public class OSSPublisher extends Publisher implements SimpleBuildStep {
         client.putObject(bucketName, realKey, inputStream);
     }
 
+    private void delete(OSSClient client, PrintStream logger, String key, boolean flag) {
+        List<String> listFiles = listFiles0(client, key);
+        if (listFiles.isEmpty()) {
+            if (flag) {
+                logger.println("file [" + key + "] not exists, skipped");
+            }
+            return;
+        }
+        for (String k : listFiles) {
+            deleteFile(client, logger, k);
+        }
+
+        delete(client, logger, key, false);
+    }
+
+    private void deleteFile(OSSClient client, PrintStream logger, String key) {
+        int maxRetries = getMaxRetries();
+        int retries = 0;
+        do {
+            if (retries > 0) {
+                logger.println("delete retrying (" + retries + "/" + maxRetries + ")");
+            }
+            try {
+                deleteFile0(client, logger, key);
+                return;
+            } catch (Exception e) {
+                e.printStackTrace(logger);
+            }
+        } while ((++retries) <= maxRetries);
+        throw new RuntimeException("delete fail, more than the max of retries");
+    }
+
+    private List<String> listFiles0(OSSClient client, String key) {
+        String realKey = key;
+        if (realKey.startsWith("/")) {
+            realKey = realKey.substring(1);
+        }
+
+        return client.listObjects(bucketName, realKey).getObjectSummaries()
+                .stream()
+                .map(OSSObjectSummary::getKey)
+                .collect(Collectors.toList());
+    }
+
+    private void deleteFile0(OSSClient client, PrintStream logger, String key) {
+        String realKey = key;
+        if (realKey.startsWith("/")) {
+            realKey = realKey.substring(1);
+        }
+
+        logger.println("deleting [" + realKey + "]");
+        client.deleteObject(bucketName, realKey);
+    }
+
     @Symbol("aliyunOSSUpload")
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
@@ -196,6 +270,13 @@ public class OSSPublisher extends Publisher implements SimpleBuildStep {
 
         public FormValidation doCheckRemotePath(@QueryParameter(required = true) String value) {
             return checkBeginWithSlash(value);
+        }
+
+        public FormValidation doCheckDeleteRemotePath(@QueryParameter String value) {
+            if (StringUtils.isNotBlank(value)) {
+                return checkBeginWithSlash(value);
+            }
+            return FormValidation.ok();
         }
 
         private FormValidation checkBeginWithSlash(String value) {
